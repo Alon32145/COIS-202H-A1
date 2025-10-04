9d14cab4434cbce4d215cc485499b41a6398217a
Alon <alonrbs@gmail.com>
2025-10-04 00:43:23 -0400

    Finished analyzer function. Code is now capable to parse digits, expressions and detect non digit or expression values and throw an error.

--- COIS-2020H-A1/PostFixExpression.cs
+++ COIS-2020H-A1/PostFixExpression.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 
 namespace Assignment1.Expressions
 {
+
+
+   
+
     public class PostFixExpression
     {
         private List<object> L;
@@ -25,7 +29,43 @@ namespace Assignment1.Expressions
             string numberAccumulator = "";
 
 
-            
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i]; // set the value of c to the current character in the string
+
+                if (char.IsDigit(c)) //if value of c is a digit add it to the numberAccumulator.
+                {
+                    // Accumulate digit for multi-digit numbers
+                    numberAccumulator += c;
+                }
+                else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')') //if c is an operator or parenthesis 
+                {
+                    if (numberAccumulator.Length > 0) // If a number was being accumulated, add it to L
+                    {
+                        L.Add(int.Parse(numberAccumulator)); // convert the accumulated string to an integer and add it to L
+                        numberAccumulator = ""; // reset the accumulator for the next number
+                    }
+                    L.Add(c);// Add operator to L
+                }
+                else if (char.IsWhiteSpace(c)) // detects whitespace
+                {
+                    if (numberAccumulator.Length > 0) // If a number was being accumulated, add it to L
+                    {
+                        L.Add(int.Parse(numberAccumulator));
+                        numberAccumulator = "";
+                    }
+                    // Ignore whitespace otherwise
+                }
+                else
+                {
+                    throw new ArgumentException($"Invalid character '{c}' in expression.");
+                }
+            }
+            if (numberAccumulator.Length > 0)// Add any remaining number at the end
+            {
+                L.Add(int.Parse(numberAccumulator));
+            }
+
         }
 
 
@@ -35,7 +75,7 @@ namespace Assignment1.Expressions
             
         }
 
-        // Evaluate: computes the result of the postfix expression
+        // Evaluate: computes the result of the postfix expression, question mark means nullable
         public float? Evaluate()
         {
             
---
+++ COIS-2020H-A1/main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Assignment1.Expressions;
+
+class main
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter an infix expression:");
+        string input = Console.ReadLine();
+
+        try
+        {
+            var expr = new PostFixExpression(input);
+            Console.WriteLine("Postfix Expression: " + expr.ToString());
+            var result = expr.Evaluate();
+            if (result.HasValue)
+                Console.WriteLine("Result: " + result.Value);
+            else
+                Console.WriteLine("Evaluation failed (syntax or runtime error).");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
